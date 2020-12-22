dc1b174b0dbfd0d24f596ff7011a1fddfcce7df6
k4keye <vps32@naver.com>
2020-12-22 18:26:47 +0900

    1,2 ok

--- src/com/company/KaKao/StringCompression/StringCompression.java
+++ src/com/company/KaKao/StringCompression/StringCompression.java
@@ -10,17 +10,26 @@ public class StringCompression {
         String[] s_arr = new String[sLength];
 
         String result =null;
-
+        int equalsCount = 1; //같은 문자 카운트
         for(int comperssion = 1; comperssion<sLength/2 ; comperssion++) //압축을 여러번 해야한다.
         {
             result ="";
-            for(int i =0 ; i<sLength;i+=comperssion)
+            for(int i =0 ; i<sLength;i+=comperssion) //한 문자씩 검사
             {
                 String sChar = s.substring(i,i+comperssion);
                 System.out.println("[기준 대상] "+ sChar);
                 System.out.println("[기준 대상 i] "+ i);
-                int equalsCount = 1; //같은 문자 카운트
-                for(int j = i+comperssion ; j<sLength ; j+=comperssion)
+
+                if((i+comperssion) >= sLength) //반복문이 동작하지못한다면
+                {
+                    if(sChar.equals(s.substring((i-comperssion),i))) equalsCount++; //이전문자 비교
+                    if(equalsCount < 2) result += sChar;
+                    else result += equalsCount+ sChar;
+                    System.out.println("마지막 >> "+result);
+
+                }
+                equalsCount = 1; //같은 문자 카운트
+                for(int j = i+comperssion ; j<sLength ; j+=comperssion) //위의 문자를 제외한 모든 문자
                 {
                     String comparison = s.substring(j,j+comperssion);
                     System.out.println("비교 "+ comparison);
@@ -29,13 +38,10 @@ public class StringCompression {
                     {
                         System.out.println("같다");
                         equalsCount++;
+
                     }
-                    if(j+comperssion >= sLength-1)
-                    {
-                        //마지막 문자열을 입력해야한다.
-                        //지금은 추가가안되고있다.
-                    }
-                    else if(!sChar.equals(comparison) )
+
+                    else if(!sChar.equals(comparison))
                     {
                         System.out.println("다르다 ");
                         if(equalsCount < 2) result += sChar;
@@ -46,6 +52,7 @@ public class StringCompression {
                         break;
                     }
                 }
+
 
             }
             System.out.println();
